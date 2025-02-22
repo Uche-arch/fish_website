f096444db67ed90238fdf9beb99f58e43e8a156d
Uche Godswill <auchjee@gmail.com>
2025-02-23 00:42:25 +0100

    Initialized JS validation

--- index.js
+++ index.js
@@ -1,44 +1,46 @@
-// function validate(){
-//     let input = document.getElementById("input").value;
-//     let error = document.getElementById("error");
-
-//     if(input == ""){
-//         error.innerHTML = "Input this field";
-//         // error.style.display = "block";
-//         // error.style.color = "red";
-//     }else {
-//         error.innerHTML = null;
-//     }
-// }
+let firstError = document.getElementById("first-error");
+let lastError = document.getElementById("last-error");
+let emailError = document.getElementById("email-error");
+let cityError = document.getElementById("city-error");
+let submitError = document.getElementById("submit-error");
 
 function validate() {
+  
   let first = document.getElementById("first").value;
   let last = document.getElementById("last").value;
   let email = document.getElementById("email").value;
+  let emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
   let city = document.getElementById("city").value;
-//   let option = document.getElementById("option").value;
-//   let btn = document.getElementById("btn").value;
 
-  let error1 = document.getElementById("error-1");
-  let error2 = document.getElementById("error-2");
-  let error3 = document.getElementById("error-3");
-  let error4 = document.getElementById("error-4");
-  let error5 = document.getElementById("error-5");
-//   let error6 = document.getElementById("error-6");
+
   if (first == "") {
-    error1.innerHTML = "Enter Firstname";
-  } else if (last == "") {
-    error2.innerHTML = "Enter Lastname";
-  } else if (email == "") {
-    error3.innerHTML = "Input Email";
-  } else if (city == "") {
-    error4.innerHTML = "Enter City name";
-  } else if (option == "") {
-    error5.innerHTML = "Pick an option";
+    firstError.innerHTML = "Input Firstname";
+  } else {
+    firstError.innerHTML = "";
   }
-  else {
-    return true
+  if (last == "") {
+    lastError.innerHTML = "Input lastname";
+  } else {
+    lastError.innerHTML = "";
   }
+  if (email == "") {
+    emailError.innerHTML = "Input Email";
+  } else if (!emailPattern.test(email)) {
+    emailError.innerHTML = "Enter a valid email address";
+  } else {
+    emailError.innerHTML = "";
+  }
+  if (city == "") {
+    cityError.innerHTML = "Input City name";
+  } else {
+    cityError.innerHTML = "";
+  }
+  if (first || last || email || city == "") {
+    submitError.innerHTML = "Please fill out all required fields";
+  } else {
+    submitError.innerHTML = "";
+  }
+  setTimeout(() => {
+    submitError.style.display = "none";
+  }, 3000);
 }
-
-// error6.innerHTML = "Fix all errors";
