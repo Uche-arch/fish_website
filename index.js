e6a78b559d5e24bbda065b9c9c5d9f58fc56725a
Uche Godswill <auchjee@gmail.com>
2025-02-23 01:05:43 +0100

    Validation for phone number

--- index.js
+++ index.js
@@ -2,16 +2,15 @@ let firstError = document.getElementById("first-error");
 let lastError = document.getElementById("last-error");
 let emailError = document.getElementById("email-error");
 let cityError = document.getElementById("city-error");
+let phoneError = document.getElementById("phone-error");
 let submitError = document.getElementById("submit-error");
 
 function validate() {
-  
   let first = document.getElementById("first").value;
   let last = document.getElementById("last").value;
   let email = document.getElementById("email").value;
   let emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
   let city = document.getElementById("city").value;
-
 
   if (first == "") {
     firstError.innerHTML = "Input Firstname";
@@ -43,4 +42,17 @@ function validate() {
   setTimeout(() => {
     submitError.style.display = "none";
   }, 3000);
+}
+
+function validatePhone (){
+  let phone = document.getElementById("phone").value;
+  if (phone.length !== 11) {
+    phoneError.innerHTML = "Enter valid phone number";
+    return false
+  }if (phone.length == 11){
+    phoneError.innerHTML = "valid phone number"
+  }
+  //  else {
+  //   phoneError.innerHTML = "";
+  // }
 }
